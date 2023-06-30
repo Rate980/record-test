8e820d7f479195045e85518c428def90c039d49c
Rate980 <xv0nnj0@icloud.com>
2023-07-01 03:34:23 +0900

    update channel

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use anyhow::Ok;
 use cpal::{
-    traits::{DeviceTrait, HostTrait},
+    traits::{DeviceTrait, HostTrait, StreamTrait},
     SampleFormat, StreamError,
 };
+use tokio::sync::mpsc::Sender;
 
-fn main() -> anyhow::Result<()> {
+async fn send(channel_tx: Sender<i16>, data: &[i16]) {
+    for sample in data {
+        channel_tx.send(*sample).await.unwrap();
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let host = cpal::default_host();
     let device = host
         .default_input_device()
@@ -18,15 +26,54 @@ fn main() -> anyhow::Result<()> {
         eprintln!("an error occurred on stream: {}", err);
     };
 
+    let (tx, mut rx) = tokio::sync::mpsc::channel::<i16>(1 << 10);
+
     let stream = match config.sample_format() {
-        SampleFormat::I8 => todo!(),
+        SampleFormat::I8 => device.build_input_stream(
+            &config.into(),
+            move |data: &[u8], _: &_| {
+                let data = data
+                    .iter()
+                    .map(|x| *x as i16)
+                    .map(|x| x << 8)
+                    .collect::<Vec<i16>>();
+                futures::executor::block_on(send(tx.clone(), &data))
+            },
+            err_fn,
+            None,
+        )?,
         SampleFormat::I16 => todo!(),
         SampleFormat::I32 => todo!(),
         SampleFormat::I64 => todo!(),
-        SampleFormat::F32 => todo!(),
+        SampleFormat::F32 => device.build_input_stream(
+            &config.into(),
+            move |data: &[f32], _: &_| {
+                let data = data
+                    .iter()
+                    .map(|x| (i16::MAX as f32) * x)
+                    .map(|x| x as i16)
+                    .collect::<Vec<i16>>();
+                futures::executor::block_on(send(tx.clone(), &data))
+            },
+            err_fn,
+            None,
+        )?,
         SampleFormat::F64 => todo!(),
         _ => return Err(anyhow::Error::msg("Unsupported sample format")),
     };
 
+    stream.play()?;
+    tokio::spawn(async move {
+        while let Some(sample) = rx.recv().await {
+            // let samples = Vec::new();
+
+            tokio::task::spawn_blocking(move || {
+                println!("{}", sample);
+            })
+            .await
+            .unwrap();
+        }
+    })
+    .await?;
     Ok(())
 }
